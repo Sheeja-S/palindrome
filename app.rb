1486e94a96b40a571c12fb6346fbddf1cca09e32
Sheeja Sankar <sheeja.s18@gmail.com>
2017-03-22 18:31:26 +0800

    removed unnecessary files

--- app.rb
+++ app.rb
@@ -1,24 +1,5 @@
 require('sinatra')
-
-class String
-  define_method(:palindromes) do
-    a= self.split('')
-    b= a.length
-    c= []
-
-    while (b >=0) do
-      c.push(a[b])
-      b -= 1
-    end
-    reversename = c.join('')
-    if(self == reversename)
-         return self + " is a palindrome"
-       else
-         return self + " is not a palindrome.You jackass"
-  end
-
- end
-end
+require('./lib/palindromes.rb')
 
 get('/') do
   erb(:home)
